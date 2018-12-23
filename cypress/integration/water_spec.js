76abea95672110ae666cd7688960009e159eb2c3
Heikki Kainulainen <hmkainul@gmail.com>
2018-12-23 16:22:29 +0200

    Add silver acetate test

--- cypress/integration/water_spec.js
+++ cypress/integration/water_spec.js
@@ -1,9 +1,18 @@
 describe('Calculator', () => {
-    it('should handle H2O', () => {
-        cy.visit('https://thisperiodictable.com')
-        cy.get('#H').click().click()
-        cy.get('#O').click()
-        cy.get('#formula_value').contains('H2 O')
-        cy.get('#mass_value').contains('18.015')
-    })
+    beforeEach(() => cy.visit('https://thisperiodictable.com'))
+    const aG = 'Ag'
+    const c = 'C'
+    const h = 'H'
+    const o = 'O'
+    it('should handle water', () =>
+        calculate([h, h, o], 'H2 O', '18.015')
+    )
+    it('should handle silver acetate', () =>
+        calculate([aG, o, o, h, h, h, c, c], 'C2 H3 Ag O2', '166.9122')
+    )
+    function calculate(clicks, formula, mass) {
+        clicks.forEach(id => cy.get('#' + id).click())
+        cy.get('#formula_value').contains(formula)
+        cy.get('#mass_value').contains(mass)
+    }
 })
